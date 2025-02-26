c969ac3e36a808ef1a9ddd8cc10f7657d4a34e70
om <omjha003@gmail.com>
2025-02-27 02:08:33 +0530

    Done with loader feature

--- Frontend/src/components/Guest/Post.jsx
+++ Frontend/src/components/Guest/Post.jsx
@@ -3,12 +3,14 @@ import { useNavigate, useParams } from "react-router-dom";
 import Navbar from "./Shared/Navbar";
 import axios from "axios";
 import { POST_API_ENDPOINT } from "@/utils/apiendpoint";
+import { Loader2 } from "lucide-react";
 
 export default function Post() {
     let params = useParams();
     let id = params.id;
     let [posts, setAllPosts] = useState([]);
     let [currPost, setCurrPost] = useState([]);
+    let [loading, setLoading] = useState(false);
     let navigate = useNavigate();
 
     // This will convert any valid date into 22 Mar 2025 type format
@@ -24,20 +26,24 @@ export default function Post() {
     useEffect(() => {
 
         let getTop9Post = async () => {
-          try {
-            let res = await axios.get(`${POST_API_ENDPOINT}/gettop9`, { withCredentials: true });
-    
-            if (res.data.success) {
-              setAllPosts(res.data.topPosts);
+            try {
+                setLoading(true);
+                let res = await axios.get(`${POST_API_ENDPOINT}/gettop9`, { withCredentials: true });
+
+                if (res.data.success) {
+                    setAllPosts(res.data.topPosts);
+                }
             }
-          }
-          catch (e) {
-            toast.error(e?.response?.data?.message);
-          }
+            catch (e) {
+                toast.error(e?.response?.data?.message);
+            }
+            finally {
+                setLoading(false);
+            }
         }
-    
+
         getTop9Post();
-      }, [])
+    }, [])
 
     useEffect(() => {
         const getCurrPost = async () => {
@@ -58,145 +64,155 @@ export default function Post() {
     }, [id]);
 
     return (
-        <div className="">
-            <Navbar />
-
-            <div className="max-w-[90%] mx-auto mt-6 p-6 rounded-xl shadow-md">
-                <h1 className="bg-gray-200 p-4 rounded-md text-xl font-semibold text-center">
-                    {currPost?.posttitle}
-                </h1>
-
-                <div className="flex flex-col gap-4 mt-6 text-gray-700">
-                    <h2>
-                        <span className="text-green-700 font-bold">Post Name:</span>&nbsp;
-                        {currPost?.postname}
-                    </h2>
-                    <h2>
-                        <span className="text-green-700 font-bold">Total Vacancy:</span>&nbsp;
-                        {currPost?.totalvacancies}
-                    </h2>
-                    <h2>
-                        <span className="text-green-700 font-bold">Latest Update:</span>&nbsp;
-                        {formatDate(currPost?.updatedAt)}
-                    </h2>
-                    <h2>
-                        <span className="text-green-700 font-bold">Post Date:</span>&nbsp;
-                        {formatDate(currPost?.createdAt)}
-                    </h2>
-                </div>
-
-                <div className="my-6">
-                    <p className="text-gray-800">
-                        <span className="text-red-600 font-bold">Brief Information:</span>&nbsp;
-                        {currPost?.briefinformation}
-                    </p>
-                </div>
-
-                {/* Box Sections */}
-                <div className="flex flex-col p-4">
-                    <h1 className="text-2xl my-4 text-center font-semibold">
-                        {currPost?.postshortname}
-                    </h1>
-
-                    <div className="grid sm:grid-cols-[repeat(auto-fit,minmax(370px,1fr))] max-sm:grid-cols-[repeat(auto-fit,minmax(370px,1fr))] gap-6 p-4">
-                        {currPost?.boxes?.slice(0, -1).map((box) => (
-                            <div key={box?.id} className="p-5 border border-gray-300 rounded-lg shadow-sm bg-gray-50" >
-                                <h2
-                                    className="text-xl font-normal text-center mb-3"
-                                    dangerouslySetInnerHTML={{ __html: box?.heading }}
-                                ></h2>
-                                <p
-                                    className="text-gray-700"
-                                    dangerouslySetInnerHTML={{ __html: box?.content }}
-                                ></p>
-                            </div>
-                        ))}
+        <div>
+            {
+                loading ? (
+                    <div className='w-full h-screen flex justify-center items-center'>
+                        <Loader2 className='animate-spin' size={25} />
                     </div>
-                </div>
+                ) : (
+                    <div className="">
+                        <Navbar />
 
-                {/* Tables Section */}
-                <div className="flex flex-col gap-8">
-                    {currPost?.tables?.map((table) => (
-                        <div key={table?.id} className="overflow-x-auto border border-gray-200 shadow-md rounded-lg" >
-                            <table className="w-full text-sm border-collapse">
-                                {/* Table Caption */}
-                                <caption className="text-lg font-semibold bg-gray-100 text-center p-4 border-gray-300">
-                                    {table?.name}
-                                </caption>
+                        <div className="max-w-[90%] mx-auto mt-6 p-6 rounded-xl shadow-md">
+                            <h1 className="bg-gray-200 p-4 rounded-md text-xl font-semibold text-center">
+                                {currPost?.posttitle}
+                            </h1>
 
-                                {/* Table Header */}
-                                <thead className="bg-blue-500 text-white uppercase text-center">
-                                    <tr>
-                                        {table?.headers?.map((header, index) => (
-                                            <th
-                                                key={index}
-                                                className="border border-gray-300 px-4 py-3 font-normal"
-                                            >
-                                                <span dangerouslySetInnerHTML={{ __html: header }}></span>
-                                            </th>
-                                        ))}
-                                    </tr>
-                                </thead>
+                            <div className="flex flex-col gap-4 mt-6 text-gray-700">
+                                <h2>
+                                    <span className="text-green-700 font-bold">Post Name:</span>&nbsp;
+                                    {currPost?.postname}
+                                </h2>
+                                <h2>
+                                    <span className="text-green-700 font-bold">Total Vacancy:</span>&nbsp;
+                                    {currPost?.totalvacancies}
+                                </h2>
+                                <h2>
+                                    <span className="text-green-700 font-bold">Latest Update:</span>&nbsp;
+                                    {formatDate(currPost?.updatedAt)}
+                                </h2>
+                                <h2>
+                                    <span className="text-green-700 font-bold">Post Date:</span>&nbsp;
+                                    {formatDate(currPost?.createdAt)}
+                                </h2>
+                            </div>
 
-                                {/* Table Body */}
-                                <tbody>
-                                    {table?.data?.map((row, rowIndex) => (
-                                        <tr
-                                            key={rowIndex}
-                                            className={`${rowIndex % 2 === 0 ? "bg-gray-50" : "bg-white"} hover:bg-gray-100 `}
-                                        >
-                                            {row?.map((cell, cellIndex) => (
-                                                <td
-                                                    key={cellIndex}
-                                                    className="border border-gray-300 px-4 py-3  "
-                                                >
-                                                    <span className="flex flex-col items-center" dangerouslySetInnerHTML={{ __html: cell }}></span>
-                                                </td>
-                                            ))}
-                                        </tr>
+                            <div className="my-6">
+                                <p className="text-gray-800">
+                                    <span className="text-red-600 font-bold">Brief Information:</span>&nbsp;
+                                    {currPost?.briefinformation}
+                                </p>
+                            </div>
+
+                            {/* Box Sections */}
+                            <div className="flex flex-col p-4">
+                                <h1 className="text-2xl my-4 text-center font-semibold">
+                                    {currPost?.postshortname}
+                                </h1>
+
+                                <div className="grid sm:grid-cols-[repeat(auto-fit,minmax(370px,1fr))] max-sm:grid-cols-[repeat(auto-fit,minmax(370px,1fr))] gap-6 p-4">
+                                    {currPost?.boxes?.slice(0, -1).map((box) => (
+                                        <div key={box?.id} className="p-5 border border-gray-300 rounded-lg shadow-sm bg-gray-50" >
+                                            <h2
+                                                className="text-xl font-normal text-center mb-3"
+                                                dangerouslySetInnerHTML={{ __html: box?.heading }}
+                                            ></h2>
+                                            <p
+                                                className="text-gray-700"
+                                                dangerouslySetInnerHTML={{ __html: box?.content }}
+                                            ></p>
+                                        </div>
                                     ))}
-                                </tbody>
-                            </table>
-                        </div>
-                    ))}
-                </div>
-
-                {/* How to fill the form Box */}
-                <div className="my-8">
-                    {
-                        currPost?.boxes?.slice(-1)?.map((box) => {
-                            return (
-                                <div key={box?.id} className="p-5 border border-gray-300 rounded-lg shadow-sm bg-gray-50" >
-                                    <h2
-                                        className="text-xl font-normal text-center mb-3"
-                                        dangerouslySetInnerHTML={{ __html: box?.heading }}
-                                    ></h2>
-                                    <p
-                                        className="text-gray-700"
-                                        dangerouslySetInnerHTML={{ __html: box?.content }}
-                                    ></p>
                                 </div>
-                            )
-                        })
-                    }
-                </div>
+                            </div>
 
-                {/* Job Notifications Section */}
-                <div className="mt-8 bg-gray-50 p-6 rounded-lg shadow-md">
-                    <h1 className="font-semibold text-lg text-gray-700">Job Notifications</h1>
-                    <ul className="mt-3 space-y-2">
-                        {posts?.map((post) => (
-                            <li
-                                key={post._id}
-                                className="text-blue-600 cursor-pointer hover:underline"
-                                onClick={() => navigate(`/post/${post._id}`)}
-                            >
-                                {post?.postname}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
+                            {/* Tables Section */}
+                            <div className="flex flex-col gap-8">
+                                {currPost?.tables?.map((table) => (
+                                    <div key={table?.id} className="overflow-x-auto border border-gray-200 shadow-md rounded-lg" >
+                                        <table className="w-full text-sm border-collapse">
+                                            {/* Table Caption */}
+                                            <caption className="text-lg font-semibold bg-gray-100 text-center p-4 border-gray-300">
+                                                {table?.name}
+                                            </caption>
+
+                                            {/* Table Header */}
+                                            <thead className="bg-blue-500 text-white uppercase text-center">
+                                                <tr>
+                                                    {table?.headers?.map((header, index) => (
+                                                        <th
+                                                            key={index}
+                                                            className="border border-gray-300 px-4 py-3 font-normal"
+                                                        >
+                                                            <span dangerouslySetInnerHTML={{ __html: header }}></span>
+                                                        </th>
+                                                    ))}
+                                                </tr>
+                                            </thead>
+
+                                            {/* Table Body */}
+                                            <tbody>
+                                                {table?.data?.map((row, rowIndex) => (
+                                                    <tr
+                                                        key={rowIndex}
+                                                        className={`${rowIndex % 2 === 0 ? "bg-gray-50" : "bg-white"} hover:bg-gray-100 `}
+                                                    >
+                                                        {row?.map((cell, cellIndex) => (
+                                                            <td
+                                                                key={cellIndex}
+                                                                className="border border-gray-300 px-4 py-3  "
+                                                            >
+                                                                <span className="flex flex-col items-center" dangerouslySetInnerHTML={{ __html: cell }}></span>
+                                                            </td>
+                                                        ))}
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                ))}
+                            </div>
+
+                            {/* How to fill the form Box */}
+                            <div className="my-8">
+                                {
+                                    currPost?.boxes?.slice(-1)?.map((box) => {
+                                        return (
+                                            <div key={box?.id} className="p-5 border border-gray-300 rounded-lg shadow-sm bg-gray-50" >
+                                                <h2
+                                                    className="text-xl font-normal text-center mb-3"
+                                                    dangerouslySetInnerHTML={{ __html: box?.heading }}
+                                                ></h2>
+                                                <p
+                                                    className="text-gray-700"
+                                                    dangerouslySetInnerHTML={{ __html: box?.content }}
+                                                ></p>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+
+                            {/* Job Notifications Section */}
+                            <div className="mt-8 bg-gray-50 p-6 rounded-lg shadow-md">
+                                <h1 className="font-semibold text-lg text-gray-700">Job Notifications</h1>
+                                <ul className="mt-3 space-y-2">
+                                    {posts?.map((post) => (
+                                        <li
+                                            key={post._id}
+                                            className="text-blue-600 cursor-pointer hover:underline"
+                                            onClick={() => navigate(`/post/${post._id}`)}
+                                        >
+                                            {post?.postname}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 }
--- Frontend/src/components/Guest/Posts.jsx
+++ Frontend/src/components/Guest/Posts.jsx
@@ -4,22 +4,26 @@ import axios from 'axios';
 import { POST_API_ENDPOINT } from '@/utils/apiendpoint';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { Loader2 } from 'lucide-react';
 
 export default function Posts() {
   let [posts, setAllPosts] = useState([]);
-  let [loading, setLoading] = useState(true);
+  let [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const getAllPosts = async () => {
       try {
+        setLoading(true);
         let res = await axios.get(`${POST_API_ENDPOINT}/get`, { withCredentials: true });
 
         if (res.data.success) {
           setAllPosts(res.data.allPosts);
         }
-      } catch (e) {
+      }
+      catch (e) {
         toast.error(e?.response?.data?.message);
-      } finally {
+      }
+      finally {
         setLoading(false);
       }
     };
@@ -36,46 +40,49 @@ export default function Posts() {
     });
   };
 
-  if (loading) {
-    return <div className="text-center py-20 text-lg">Loading...</div>;
-  }
-
-  if (posts.length === 0) {
-    return <div className="text-center py-20 text-lg text-gray-500">No posts available</div>;
-  }
-
   return (
     <div>
-      <Navbar />
-      <div className="w-[85%] mx-auto my-8">
-        <h2 className="md:text-3xl max-md:text-2xl max-sm:text-xl font-bold mb-6 text-gray-800">All {posts.length} Posts</h2>
+      {
+        loading ? (
+          <div className='w-full h-screen flex justify-center items-center'>
+            <Loader2 className='animate-spin' size={25} />
+          </div>
+        ) : (
+          <div>
+            <Navbar />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:gap-8 max-sm:gap-3">
-          {posts.map((post) => (
-            <div
-              key={post._id}
-              className="bg-white border border-gray-300 p-6 max-sm:p-4 rounded-xl shadow-md hover:shadow-2xl transition-all ease-in-out duration-300 transform hover:scale-105"
-            >
-              <Link
-                to={`/post/${post._id}`}
-                className="text-xl font-semibold text-blue-600 hover:text-blue-800 transition-all ease-in-out duration-200 max-sm:text-sm"
-              >
-                {post?.postname}
-              </Link>
-              <div className="mt-4 sm:mt-2 text-gray-700">
-                <p>
-                  <span className="font-medium text-sm">Last Date: </span>
-                  <span className="text-red-600 text-sm">{formatDate(post.endingdate)}</span>
-                </p>
-                <p>
-                  <span className="font-medium text-sm">Total Vacancies: </span>
-                  <span className="text-sm">{post?.totalvacancies}</span>
-                </p>
+            <div className="w-[85%] mx-auto my-8">
+              <h2 className="md:text-3xl max-md:text-2xl max-sm:text-xl font-bold mb-6 text-gray-800">All {posts.length} Posts</h2>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:gap-8 max-sm:gap-3">
+                {posts.map((post) => (
+                  <div
+                    key={post._id}
+                    className="bg-white border border-gray-300 p-6 max-sm:p-4 rounded-xl shadow-md hover:shadow-2xl transition-all ease-in-out duration-300 transform hover:scale-105"
+                  >
+                    <Link
+                      to={`/post/${post._id}`}
+                      className="text-xl font-semibold text-blue-600 hover:text-blue-800 transition-all ease-in-out duration-200 max-sm:text-sm"
+                    >
+                      {post?.postname}
+                    </Link>
+                    <div className="mt-4 sm:mt-2 text-gray-700">
+                      <p>
+                        <span className="font-medium text-sm">Last Date: </span>
+                        <span className="text-red-600 text-sm">{formatDate(post.endingdate)}</span>
+                      </p>
+                      <p>
+                        <span className="font-medium text-sm">Total Vacancies: </span>
+                        <span className="text-sm">{post?.totalvacancies}</span>
+                      </p>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
+        )
+      }
     </div>
   );
 }
