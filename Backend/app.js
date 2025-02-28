de24904ed68dcd1efb7e9a941726b223a81e694b
om <omjha003@gmail.com>
2025-03-01 00:26:29 +0530

    Replaced fronend local url to render url

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ let port = process.env.PORT || 4000
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 const corsOptions = {
-    origin : "http://localhost:5173",
+    origin : "https://jhatutorialsmern-frontend.onrender.com/",
     credentials : true
 }
 
--- Frontend/src/components/Guest/Posts.jsx
+++ Frontend/src/components/Guest/Posts.jsx
@@ -32,10 +32,6 @@ export default function Posts() {
     getAllPosts();
   }, []);
 
-  if (error) {
-    return <InternalServerError />
-  }
-
   const formatDate = (dateStr) => {
     const date = new Date(dateStr);
     return date.toLocaleDateString('en-GB', {
@@ -50,54 +46,58 @@ export default function Posts() {
     const now = new Date();
     const diffInHours = (now - postTime) / (1000 * 60 * 60); // Convert milliseconds to hours
     return diffInHours <= 6; // Returns true if within 6 hours
-};
+  };
 
   return (
     <div>
-      {
-        loading ? (
-          <div className='w-full h-screen flex justify-center items-center'>
-            <Loader2 className='animate-spin' size={25} />
-          </div>
-        ) : (
-          <div>
-            <Navbar />
+      {error ? (
+        <InternalServerError />
+      ) : loading ? (
+        <div className='w-full h-screen flex justify-center items-center'>
+          <Loader2 className='animate-spin' size={25} />
+        </div>
+      ) : (
+        <div>
+          <Navbar />
+          <div className="w-[85%] mx-auto my-8">
+            <h2 className="md:text-3xl max-md:text-2xl max-sm:text-xl font-bold mb-6 text-gray-800">
+              All {posts.length} Posts
+            </h2>
 
-            <div className="w-[85%] mx-auto my-8">
-              <h2 className="md:text-3xl max-md:text-2xl max-sm:text-xl font-bold mb-6 text-gray-800">All {posts.length} Posts</h2>
-
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:gap-8 max-sm:gap-3">
-                {posts.map((post) => (
-                  <div
-                    key={post._id}
-                    className="bg-white border relative border-gray-300 p-6 max-sm:p-4 rounded-xl shadow-md hover:shadow-2xl transition-all ease-in-out duration-300 transform hover:scale-105"
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:gap-8 max-sm:gap-3">
+              {posts.map((post) => (
+                <div
+                  key={post._id}
+                  className="bg-white border relative border-gray-300 p-6 max-sm:p-4 rounded-xl shadow-md hover:shadow-2xl transition-all ease-in-out duration-300 transform hover:scale-105"
+                >
+                  <Link
+                    to={`/post/${post._id}`}
+                    className="text-xl font-semibold text-blue-600 hover:text-blue-800 transition-all ease-in-out duration-200 max-sm:text-sm"
                   >
-                    <Link
-                      to={`/post/${post._id}`}
-                      className="text-xl font-semibold text-blue-600 hover:text-blue-800 transition-all ease-in-out duration-200 max-sm:text-sm"
-                    >
-                      {post?.postname}
-                    </Link>
-                    {isNewPost(post.createdAt) && ( // Display posts with new badge if is created within 6 hours.
-                      <span className="bg-red-500 absolute left-[-15px] top-[-8px] text-white text-xs font-bold px-2 py-1 rounded">New</span>
-                    )}
-                    <div className="mt-4 sm:mt-2 text-gray-700">
-                      <p>
-                        <span className="font-medium text-sm">Last Date: </span>
-                        <span className="text-red-600 text-sm">{formatDate(post.endingdate)}</span>
-                      </p>
-                      <p>
-                        <span className="font-medium text-sm">Total Vacancies: </span>
-                        <span className="text-sm">{post?.totalvacancies}</span>
-                      </p>
-                    </div>
+                    {post?.postname}
+                  </Link>
+                  {isNewPost(post.createdAt) && (
+                    <span className="bg-red-500 absolute left-[-15px] top-[-8px] text-white text-xs font-bold px-2 py-1 rounded">
+                      New
+                    </span>
+                  )}
+                  <div className="mt-4 sm:mt-2 text-gray-700">
+                    <p>
+                      <span className="font-medium text-sm">Last Date: </span>
+                      <span className="text-red-600 text-sm">{formatDate(post.endingdate)}</span>
+                    </p>
+                    <p>
+                      <span className="font-medium text-sm">Total Vacancies: </span>
+                      <span className="text-sm">{post?.totalvacancies}</span>
+                    </p>
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
           </div>
-        )
-      }
+        </div>
+      )}
     </div>
   );
+
 }
