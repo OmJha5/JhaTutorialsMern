61c282d7de4083984bad2155d7ade32a0ab5467a
om <omjha003@gmail.com>
2025-03-10 09:08:09 +0530

    Fixed few changes

--- Frontend/src/components/Admin/CreateAdmitCard.jsx
+++ Frontend/src/components/Admin/CreateAdmitCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import JobCommonInfo from './PostCommonInfo'
 import { ArrowLeft, Loader2 } from "lucide-react";
 import { Button } from '../ui/button';
